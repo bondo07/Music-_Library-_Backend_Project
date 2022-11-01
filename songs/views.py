c813c70fd2d79c6ceff6d403ad10d7588d2f169c
Armando Beltran <armando_b@live.com>
2022-11-01 16:23:03 -0500

    Created Delete Functionality for Song Views File + Exported Postman Collection to Project folder

--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,9 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SongSerializer
 from .models import Song
-from songs import serializers
 
 
 
@@ -19,11 +19,17 @@ def songs_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT','DELETE'])
 def songs_detail(request, pk):
-    try:
-        song = Song.objects.get(pk=pk)
+    song = get_object_or_404(Song, pk=pk)
+    if request.method == 'GET':
         serializer = SongSerializer(song)
         return Response(serializer.data)
-    except Song.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+    elif request.method =='PUT':
+        serializer = SongSerializer(song, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        song.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
