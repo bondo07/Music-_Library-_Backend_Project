3b82fd76873c685e9bd706e43b99fe8737c35b73
Armando Beltran <armando_b@live.com>
2022-11-01 13:30:22 -0500

    Created Song Serializer & Class

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -35,9 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'corsheaders',
     'songs',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
