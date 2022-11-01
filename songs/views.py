9c205333b5bbd100c831fdeea8ef9f7f52780431
Armando Beltran <armando_b@live.com>
2022-11-01 15:03:47 -0500

    Created New View Function for Songs_By_Id + Added New URLs path for songs_detail View Function

--- songs/views.py
+++ songs/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import SongSerializer
 from .models import Song
+from songs import serializers
 
 
 
@@ -18,3 +19,11 @@ def songs_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+@api_view(['GET'])
+def songs_detail(request, pk):
+    try:
+        song = Song.objects.get(pk=pk)
+        serializer = SongSerializer(song)
+        return Response(serializer.data)
+    except Song.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
